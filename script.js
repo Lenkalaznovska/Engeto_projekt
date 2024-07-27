4b08b601311cdd6237bdaf25a4126205230ef620
Lenka Lážnovská <159648392+Lenkalaznovska@users.noreply.github.com>
2024-07-27 20:07:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const themeSwitcher = document.querySelector(
-    '.switch input[type="checkbox"]'
-  );
+  const themeSwitcher = document.querySelector('.switch input[type="checkbox"]');
   const body = document.body;
   const menuToggle = document.querySelector(".menu-toggle");
   const menu = document.querySelector(".menu");
   const backToTopButton = document.getElementById("backToTop");
   const loginForm = document.getElementById("loginForm");
 
-  // Přepínač tmavého režimu
+  // Dark mode toggle switch
   themeSwitcher.addEventListener("change", function () {
     if (this.checked) {
       body.setAttribute("data-theme", "dark");
@@ -17,12 +15,12 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 
-  // Přepínač mobilního menu
+  // Mobile menu toggle switch
   menuToggle.addEventListener("click", function () {
     menu.classList.toggle("show");
   });
 
-  // Tlačítko pro návrat na začátek
+  // Back-to-top button
   window.addEventListener("scroll", function () {
     if (window.scrollY > 200) {
       backToTopButton.style.display = "block";
@@ -35,16 +33,16 @@ document.addEventListener("DOMContentLoaded", function () {
     window.scrollTo({ top: 0, behavior: "smooth" });
   });
 
-  // Validace formuláře
+  // Form validation
   loginForm.addEventListener("submit", function (event) {
     event.preventDefault();
     const password = document.getElementById("password").value;
     const confirmPassword = document.getElementById("password-confirm").value;
 
     if (password !== confirmPassword) {
-      alert("Hesla se neshodují!");
+      alert("Passwords do not match!");
     } else {
-      alert("Přihlášení bylo úspěšné!");
+      alert("Login successful!");
     }
   });
 });
